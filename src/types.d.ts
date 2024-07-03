6903752f2660e365666e2524c340ddb178fb50b6
ecaleb97 <valley_rob.0c@icloud.com>
2024-07-03 23:12:37 +0200

    chore: Update types for resume data and contact information

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,54 +1,54 @@
 export interface PortfolioInfo {
-  name: string;
-  url: string;
+	name: string;
+	url: string;
 }
 
 export interface ContactLink {
-  name: string;
-  url: string;
+	name: string;
+	url: string;
 }
 
 export interface ContactEmail {
-  name: string;
-  at: string;
+	name: string;
+	at: string;
 }
 
 export interface SocialLink {
-  name: string;
-  url: string;
-  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+	name: string;
+	url: string;
+	icon: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
 export interface ContactInfo {
-  link: ContactLink
-  email: ContactEmail;
-  social: Array<SocialLink>;
+	link: ContactLink;
+	email: ContactEmail;
+	social: Array<SocialLink>;
 }
 
 export interface Education {
-  school: string;
-  degree: string;
-  from: string;
-  to: string;
+	school: string;
+	degree: string;
+	from: string;
+	to: string;
 }
 
 export interface Work {
-  company: string;
-  position: string;
-  from: string;
-  to: string;
-  description: string;
+	company: string;
+	position: string;
+	from: string;
+	to: string;
+	description: string;
 }
 
 export interface ResumeData {
-  name: string;
-  initials: string;
-  location: string;
-  about: string;
-  summary: string;
-  avatar: string;
-  portfolioInfo: PortfolioInfo;
-  contact: ContactInfo;
-  education: Array<Education>;
-  work: Array<Work>;
+	name: string;
+	initials: string;
+	location: string;
+	about: string;
+	summary: string;
+	avatar: string;
+	portfolioInfo: PortfolioInfo;
+	contact: ContactInfo;
+	education: Array<Education>;
+	work: Array<Work>;
 }
