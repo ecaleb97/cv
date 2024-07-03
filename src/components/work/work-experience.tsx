168114e3930ad1b4599824af84d1ad6d97d542e0
ecaleb97 <valley_rob.0c@icloud.com>
2024-07-03 23:19:22 +0200

    chore: Refactor WorkExperience component for improved code readability and formatting

--- src/components/work/work-experience.tsx
+++ src/components/work/work-experience.tsx
@@ -1,36 +1,28 @@
 import { ResumeData } from "@/types";
 import { SectionHeading } from "../heading/section-heading";
 
-export function WorkExperience({
-  data
-}: {
-  data: ResumeData
-}) {
-  return (
-    <section className="space-y-4">
-      <SectionHeading name="Work Experience" />
-      <ul className="space-y-4">
-        {data.work.map((experience) => (
-          <li key={experience.company} className="space-y-1">
-            <div className="flex items-center justify-between">
-              <h3 className="font-semibold tracking-wide">{experience.company}</h3>
-              <p className="font-sans text-muted-foreground">
-                {experience.from} - {experience.to}
-              </p>
-            </div>
-            <h4 
-              className="font-mono text-sm"
-            >
-              {experience.position}
-            </h4>
-            <p 
-              className="font-mono text-sm text-muted-foreground"
-            >
-              {experience.description}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </section>
-  )
+export function WorkExperience({ data }: { data: ResumeData }) {
+	return (
+		<section className="space-y-4">
+			<SectionHeading name="Work Experience" />
+			<ul className="space-y-4">
+				{data.work.map((experience) => (
+					<li key={experience.description} className="space-y-1">
+						<div className="flex items-center justify-between">
+							<h3 className="font-semibold tracking-wide">
+								{experience.company}
+							</h3>
+							<p className="text-muted-foreground">
+								{experience.from} - {experience.to}
+							</p>
+						</div>
+						<h4 className="text-sm">{experience.position}</h4>
+						<p className="text-sm text-muted-foreground">
+							{experience.description}
+						</p>
+					</li>
+				))}
+			</ul>
+		</section>
+	);
 }
